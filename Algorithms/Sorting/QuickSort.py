cc26dd822e37e1521a5daedbde3d1f7146e26fa7
Jesse-L2 <jesse.little@utexas.edu>
2024-09-16 10:54:41 -0500

    QuickSort algo

---
+++ Algorithms/Sorting/QuickSort.py
@@ -0,0 +1,27 @@
+def quick_sort(arr: list[int], start, end) -> list[int]:
+    # Pick a pivot, usually the last value as we will do here
+    # Any value less than or equal to pivot gets put in left side
+    # Other values (bigger values) go in the right side
+    # Perform in place, O(1) space
+    if end - start + 1 <= 1:
+        return arr
+    pivot = arr[end]
+    left = start
+
+    # Partition
+    for i in range(start, end):
+        if arr[i] <= pivot:
+            temp = arr[left]
+            arr[left] = arr[i]
+            arr[i] = temp
+            left += 1
+
+    arr[end] = arr[left]
+    arr[left] = pivot
+
+    # Sort left
+    quick_sort(arr, start, left - 1)
+    # Sort right
+    quick_sort(arr, left + 1, end)
+
+    return arr
