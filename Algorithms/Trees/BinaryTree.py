eeb43f3e2eeb808b29532b819771098c528ab14d
Jesse-L2 <jesse.little@utexas.edu>
2024-09-25 12:53:01 -0500

    Add TreeNode class to BinaryTree

---
+++ Algorithms/Trees/BinaryTree.py
@@ -0,0 +1,12 @@
+"""
+Each node in the tree will have two pointers to its children, one on the left and one on the right.
+The final node (leaf) of the tree will still have two pointers, but to null rather than other nodes
+Height - path from node to deepest leaf
+Depth - path from node to root node
+"""
+
+class TreeNode():
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
