ab62a3ab1e7e727cd3e219b17dff1fc4daa2ebae
Jesse-L2 <jesse.little@utexas.edu>
2024-10-28 15:23:09 -0500

    Add Backtracking and SetsMaps in Trees

---
+++ Algorithms/Trees/Backtracking.py
@@ -0,0 +1,1 @@
+# Determine if a path exists from the root of the tree to a leaf node. Return True or False, it may not contain any zeroes
--- Algorithms/Trees/SetsMaps.py
+++ Algorithms/Trees/SetsMaps.py
@@ -1,11 +1,11 @@
 # commonly implemented with a Binary Search Tree
 
 
-    """
-    Python:
-    from sortedcontainers import SortedDict
-    treeMap = SortedDict({'a': 1, 'b': 2, 'c': 3})
-    JS:
-    const TreeMap = require("treemap-js");
-    var map = new TreeMap();
-    """
+"""
+Python:
+from sortedcontainers import SortedDict
+treeMap = SortedDict({'a': 1, 'b': 2, 'c': 3})
+JS:
+const TreeMap = require("treemap-js");
+var map = new TreeMap();
+"""
