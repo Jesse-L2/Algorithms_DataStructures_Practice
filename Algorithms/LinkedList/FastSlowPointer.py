55ad50169469e643ab1fbe0dfdd77fea092201e6
Jesse-L2 <jesse.little@utexas.edu>
2024-12-04 12:35:21 -0600

    More fast/slow pointer algorithms

--- Algorithms/LinkedList/FastSlowPointer.py
+++ Algorithms/LinkedList/FastSlowPointer.py
@@ -6,3 +6,33 @@ def middleOfLinkedList(head):
         slow = slow.next
         fast = fast.next.next
     return slow
+
+# Determine if a linked list has a cycle (revisits the same node twice)
+def hasCycle(head):
+    # Time: O(n) | Space: O(1)
+    slow, fast = head, head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+# Determine if a linked list has a cycle and return the head of the cycle else None
+def hasCycleHead(head):
+    slow, fast, slow2 = head, head, head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+
+    if not fast or not fast.next: # reached end of linked list
+        return None
+    
+    while slow != slow2:
+        slow = slow.next
+        slow2 = slow2.next
+    return slow
+        
+
