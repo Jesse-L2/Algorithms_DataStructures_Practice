ef6a61e92010f5c48568b12808b7f154461540be
Jesse-L2 <jesse.little@utexas.edu>
2024-12-12 11:05:41 -0600

    Finish SegmentTree.py

--- Algorithms/Trees/SegmentTree.py
+++ Algorithms/Trees/SegmentTree.py
@@ -24,3 +24,28 @@ class SegmentTree: # implemented with linked pointers, but could also be done wi
         root.right = SegmentTree.build(nums, M + 1, R)
         root.sum = root.left.sum + root.right.sum
         return root
+    
+    def update(self, index, val): # O(log n) time complexity
+        if self.L == self.R:
+            self.sum = val
+            return
+        
+        M = (self.L + self.R) // 2
+
+        if index > M:
+            self.right.update(index, val)
+        else:
+            self.left.update(index, val)
+        self.sum = self.left.sum + self.right.sum # O(1)
+
+    def rangeQuery(self, L, R): # O(log n) time complexity
+        if L == self.L and R == self.R:
+            return self.sum
+        
+        M = (self.L + self.R) // 2
+        if L > M: 
+            return self.right.rangeQuery(L, R)
+        elif R <= M:
+            return self.left.rangeQuery(L, R)
+        else: # range falls split between both sides of tree so have to go to both sides and add together
+            return (self.left.rangeQuery(L, M) + self.right.rangeQuery(M + 1, R))
